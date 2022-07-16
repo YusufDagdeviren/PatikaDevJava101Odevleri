cfb451af8e9ce8e31032c794d2c6459e4780e673
YazilimOfisim <87547919+xSentez@users.noreply.github.com>
2022-07-16 20:51:48 +0300

    commiiiittitit

--- Fibonacci_Hesapla/Fibonacci.java
+++ Fibonacci_Hesapla/Fibonacci.java
@@ -16,13 +16,13 @@ public class Fibonacci {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lutfen fibonaccisini hesaplayacaginiz diziyi giriniz: ");
         int fib = scanner.nextInt();
-        int a=0,b=1,c;
-        System.out.print(a+" "+b+" ");
-        for(int i=0;i<fib-1;i++){
-            c=a+b;
-            System.out.print(c+" ");
-            a=b;
-            b=c;
+        int a = 0, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 0; i < fib - 1; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
         }
     }
 }
