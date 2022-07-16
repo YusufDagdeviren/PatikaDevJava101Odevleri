04ec325b9a6f68e028a4ac249fda206fd2632192
YazilimOfisim <87547919+xSentez@users.noreply.github.com>
2022-07-16 20:51:22 +0300

    commiiiittitit

---
+++ Fibonacci_Hesapla/Fibonacci.java
@@ -0,0 +1,28 @@
+package Fibonacci_Hesapla;
+
+import java.util.Scanner;
+
+/*
+ * Fibonacci Serisi Nedir ?
+Fibonacci serisi, her sayının kendinden önceki ile toplanması sonucu oluşan bir sayı dizisidir.
+ Bu şekilde devam eden bu dizide sayılar birbirleriyle oranlandığında altın oran ortaya çıkar,
+  yani bir sayı kendisinden önceki sayıya bölündüğünde altın orana gittikçe yaklaşan bir dizi elde edilir.
+
+Fibonacci dizisi, 0'dan başlar ve sonsuza kadar. Her rakam, bir önceki rakamla toplanır. 
+Elde edilen sonuç rakamın sağ tarafına yazılır. Fibonacci dizisinin ilk on sayısı şu şekildedir:
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen fibonaccisini hesaplayacaginiz diziyi giriniz: ");
+        int fib = scanner.nextInt();
+        int a=0,b=1,c;
+        System.out.print(a+" "+b+" ");
+        for(int i=0;i<fib-1;i++){
+            c=a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+    }
+}
