306d88199398a3c13e05673f28d27cc8067098c2
YazilimOfisim <87547919+xSentez@users.noreply.github.com>
2022-07-16 19:24:42 +0300

    committt

---
+++ Yildiz_Ile_Elmas/Elmas.java
@@ -0,0 +1,45 @@
+package Yildiz_Ile_Elmas;
+
+import java.util.Scanner;
+
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+ */
+public class Elmas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen bir sayi giriniz: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i=0;i<=n;i++){
+            for (int j=n; j>(n-i);j--){
+                System.out.print(" ");
+            }
+            for (int k=1; k<=(2*(n-i)-1);k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+       
+
+
+
+
+    }
+}
