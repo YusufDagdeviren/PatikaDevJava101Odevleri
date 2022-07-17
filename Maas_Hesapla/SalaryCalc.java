a4a1610c140a6f7a78e29757305cb6cffa06cf57
YazilimOfisim <87547919+xSentez@users.noreply.github.com>
2022-07-17 15:52:53 +0300

    commit

---
+++ Maas_Hesapla/SalaryCalc.java
@@ -0,0 +1,8 @@
+package Maas_Hesapla;
+public class SalaryCalc {
+    public static void main(String[] args) throws Exception {
+        Employee e1 = new Employee("Hüseyin ÇATALBAŞ", 2000, 45, 1985);
+        e1.printEmployee();
+    }
+}
+
